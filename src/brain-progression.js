a9c4c3e82d4c9398a2e814b81a1b9aef798f5f0d
Igor <trololol-lolo@mail.ru>
2024-08-27 22:06:49 +1000

    fix: Небольшой рефакторинг, вынес функцию создания прогрессии

--- src/brain-progression.js
+++ src/brain-progression.js
@@ -3,26 +3,28 @@ import { getRandomNum, getAnswer, getQuestion, compareResult } from './index.js'
 
 const name = askName();
 
+const getProgression = (length, step, start) => {
+  const progressionArray = [];
+  for (let i = 0; i < length; i += 1) {
+    const value = start + (step * i);
+    progressionArray.push(value);
+  }
+  return progressionArray;
+};
+
 export default () => {
   getQuestion('progressionGame');
   for (let i = 0; i < 3; i += 1) {
     const progressionLength = getRandomNum(5, 15);
     const progressionIndex = getRandomNum(0, progressionLength - 1);
     const progressionStep = getRandomNum(1, 5);
-    const progressionArray = [];
-    let element = getRandomNum();
-    let result;
-    for (let j = 0; j < progressionLength; j += 1) {
-      progressionArray.push(element + progressionStep);
-      element += progressionStep;
-      if (j === progressionIndex) {
-        progressionArray.push(' .. ');
-        element += progressionStep;
-        result = element;
-      }
-    }
+    const progressionStart = getRandomNum();
+    const progressionArray = getProgression(progressionLength, progressionStep, progressionStart);
+    const result = progressionArray[progressionIndex];
+    progressionArray[progressionIndex] = '..';
+
     const answer = getAnswer(progressionArray.join(' '), 'progressionGame');
-    if (compareResult(answer, result, name) === false) {
+    if (compareResult(answer, result.toString(), name) === false) {
       return null;
     };
   }
