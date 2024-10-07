97a134d857090f4837678f9e60f86592aa0d9261
Igor <trololol-lolo@mail.ru>
2024-10-07 15:31:25 +1000

    fix: Переработка игры нахождения НОД

--- src/brain-gcd.js
+++ src/brain-gcd.js
@@ -1,25 +1,40 @@
-import askName from './cli.js';
+// import askName from './cli.js';
 import {
-  getRandomNum, getAnswer, getQuestion, compareResult,
+  getRandomNum,
+  gameEngine,
 } from './index.js';
 
-const name = askName();
+// const name = askName();
+const game = 'Find the greatest common divisor of given numbers.';
 
 function gcdFind(num, num2) {
   return num2 === 0 ? num : gcdFind(num2, num % num2);
 }
 
-export default () => {
-  getQuestion('gcdGame');
-  for (let i = 0; i < 3; i += 1) {
-    const num = getRandomNum();
-    const num2 = getRandomNum();
-    const answer = getAnswer(num, 'gcdGame', num2);
-    const result = gcdFind(num, num2);
+const getQuestionAndAnswer = () => {
+  const num1 = getRandomNum();
+  const num2 = getRandomNum();
+  const question = `${num1} ${num2}`;
+  const answer = gcdFind(num1, num2);
 
-    if (compareResult(answer, result, name) === false) {
-      return null;
-    }
-  }
-  return console.log(`Congratulations, ${name}!`);
+  return [question, answer];
 };
+
+export default () => {
+  gameEngine(game, getQuestionAndAnswer);
+};
+
+// export default () => {
+//   getQuestion('gcdGame');
+//   for (let i = 0; i < 3; i += 1) {
+//     const num = getRandomNum();
+//     const num2 = getRandomNum();
+//     const answer = getAnswer(num, 'gcdGame', num2);
+//     const result = gcdFind(num, num2);
+
+//     if (compareResult(answer, result, name) === false) {
+//       return null;
+//     }
+//   }
+//   return console.log(`Congratulations, ${name}!`);
+// };
