c53c41bb3464baa1dd09114c3872da99e074864b
Igor <trololol-lolo@mail.ru>
2024-10-07 15:15:34 +1000

    fix: Переработка игры калькулятор

--- src/brain-calc.js
+++ src/brain-calc.js
@@ -1,9 +1,12 @@
-import askName from './cli.js';
+// import askName from './cli.js';
 import {
-  getAnswer, getQuestion, getRandomNum, compareResult,
+  getRandomNum,
+  gameEngine,
 } from './index.js';
 
-const name = askName();
+// const name = askName();
+
+const game = 'What is the result of the expression?';
 
 const getRandomSign = () => {
   const arrSign = ['+', '-', '*'];
@@ -21,20 +24,34 @@ const checkResult = (num1, num2, sign) => {
     result = num1 * num2;
   }
 
-  return result;
+  return result.toString();
+};
+
+const getQuestionAndAnswer = () => {
+  const num1 = getRandomNum();
+  const num2 = getRandomNum();
+  const sign = getRandomSign();
+  const question = `${num1} ${sign} ${num2}`;
+  const answer = checkResult(num1, num2, sign);
+
+  return [question, answer];
 };
 
 export default () => {
-  getQuestion('calcGame');
-  for (let i = 0; i < 3; i += 1) {
-    const num = getRandomNum();
-    const num2 = getRandomNum();
-    const sign = getRandomSign();
-    const answer = getAnswer(num, 'calcGame', sign, num2);
-    const result = checkResult(num, num2, sign);
-    if (compareResult(answer, result.toString(), name) === false) {
-      return null;
-    }
-  }
-  return console.log(`Congratulations, ${name}!`);
+  gameEngine(game, getQuestionAndAnswer);
 };
+
+// export default () => {
+//   getQuestion('calcGame');
+//   for (let i = 0; i < 3; i += 1) {
+//     const num = getRandomNum();
+//     const num2 = getRandomNum();
+//     const sign = getRandomSign();
+//     const answer = getAnswer(num, 'calcGame', sign, num2);
+//     const result = checkResult(num, num2, sign);
+//     if (compareResult(answer, result.toString(), name) === false) {
+//       return null;
+//     }
+//   }
+//   return console.log(`Congratulations, ${name}!`);
+// };
