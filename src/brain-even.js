d8e6a96f7c2d602147ee450eb2a4402e50437f3d
Igor <trololol-lolo@mail.ru>
2024-10-07 13:51:07 +1000

    fix: Создание функции-движка игры и перенос игры на проверку чётности

--- src/brain-even.js
+++ src/brain-even.js
@@ -1,9 +1,12 @@
 import askName from './cli.js';
 import {
-  getRandomNum, getAnswer, getQuestion, compareResult,
+  getRandomNum,
+  gameEngine,
 } from './index.js';
 
-const name = askName();
+// const name = askName();
+const game = 'Answer "yes" if the number is even, otherwise answer "no".';
+
 const isEven = (number) => {
   if (number % 2 === 0) {
     return 'yes';
@@ -11,15 +14,30 @@ const isEven = (number) => {
   return 'no';
 };
 
-export default () => {
-  getQuestion('evenGame');
-  for (let i = 0; i < 3; i += 1) {
-    const num = getRandomNum();
-    const answer = getAnswer(num, 'evenGame');
-    const result = isEven(num);
-    if (compareResult(answer, result, name) === false) {
-      return null;
-    }
-  }
-  return console.log(`Congratulations, ${name}!`);
+const getQuestionAndAnswer = () => {
+  const question = getRandomNum();
+  const answer = isEven(question);
+  return [question, answer];
 };
+
+// const getAnswer = () => {
+//   const answer = isEven(getQuestion()) ? 'yes' : 'no';
+//   return answer;
+// };
+
+export default () => {
+  gameEngine(game, getQuestionAndAnswer);
+};
+
+// export default () => {
+//   getQuestion('evenGame');
+//   for (let i = 0; i < 3; i += 1) {
+//     const num = getRandomNum();
+//     const answer = getAnswer(num, 'evenGame');
+//     const result = isEven(num);
+//     if (compareResult(answer, result, name) === false) {
+//       return null;
+//     }
+//   }
+//   return console.log(`Congratulations, ${name}!`);
+// };
--- src/index.js
+++ src/index.js
@@ -1,50 +1,73 @@
 import readlineSync from 'readline-sync';
+import askName from './cli.js';
 
 export function getRandomNum(min = 0, max = 100) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-export const getQuestion = (type) => {
-  if (type === 'evenGame') {
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  } else if (type === 'calcGame') {
-    console.log('What is the result of the expression?');
-  } else if (type === 'gcdGame') {
-    console.log('Find the greatest common divisor of given numbers.');
-  } else if (type === 'progressionGame') {
-    console.log('What number is missing in the progression?');
-  } else if (type === 'primeGame') {
-    console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-  }
-};
+// export const getQuestion = (type) => {
+//   if (type === 'evenGame') {
+//     console.log('Answer "yes" if the number is even, otherwise answer "no".');
+//   } else if (type === 'calcGame') {
+//     console.log('What is the result of the expression?');
+//   } else if (type === 'gcdGame') {
+//     console.log('Find the greatest common divisor of given numbers.');
+//   } else if (type === 'progressionGame') {
+//     console.log('What number is missing in the progression?');
+//   } else if (type === 'primeGame') {
+//     console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+//   }
+// };
 
-export const getAnswer = (num, type, ...arg) => {
-  let answer;
-  if (type === 'evenGame') {
-    answer = readlineSync.question(`Question: ${num} `);
-    console.log(`Your answer: ${answer}`);
-  } else if (type === 'calcGame') {
-    answer = readlineSync.question(`Question: ${num} ${arg[0]} ${arg[1]} `);
-    console.log(`Your answer: ${answer}`);
-  } else if (type === 'gcdGame') {
-    answer = readlineSync.question(`Question: ${num} ${arg[0]} `);
-    console.log(`Your answer: ${answer}`);
-  } else if (type === 'progressionGame') {
-    answer = readlineSync.question(`Question: ${num} `);
-    console.log(`Your answer: ${answer}`);
-  } else if (type === 'primeGame') {
-    answer = readlineSync.question(`Question: ${num} `);
-    console.log(`Your answer: ${answer}`);
-  }
-  return answer;
-};
+// export const getAnswer = (num, type, ...arg) => {
+//   let answer;
+//   if (type === 'evenGame') {
+//     answer = readlineSync.question(`Question: ${num} `);
+//     console.log(`Your answer: ${answer}`);
+//   } else if (type === 'calcGame') {
+//     answer = readlineSync.question(`Question: ${num} ${arg[0]} ${arg[1]} `);
+//     console.log(`Your answer: ${answer}`);
+//   } else if (type === 'gcdGame') {
+//     answer = readlineSync.question(`Question: ${num} ${arg[0]} `);
+//     console.log(`Your answer: ${answer}`);
+//   } else if (type === 'progressionGame') {
+//     answer = readlineSync.question(`Question: ${num} `);
+//     console.log(`Your answer: ${answer}`);
+//   } else if (type === 'primeGame') {
+//     answer = readlineSync.question(`Question: ${num} `);
+//     console.log(`Your answer: ${answer}`);
+//   }
+//   return answer;
+// };
 
-export const compareResult = (answer, result, name) => {
-  if (answer === result.toString()) {
+// export const compareResult = (answer, result, name) => {
+//   if (answer === result.toString()) {
+//     console.log('Correct!');
+//     return true;
+//   }
+//   console.log(`${answer} is wrong answer ;(. Correct answer was ${result}`);
+//   console.log(`Let's try again, ${name}!`);
+//   return false;
+// };
+
+export const gameEngine = (game, getQuestionAndAnswer) => {
+  const name = askName();
+  console.log(game);
+  const roundCount = 3;
+  for (let round = 0; round < roundCount; round += 1) {
+    // const question = getQuestion();
+    // const correctAnswer = getAnswer();
+    const [question, correctAnswer] = getQuestionAndAnswer();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (correctAnswer !== userAnswer) {
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}`);
+      console.log(`Let's try again, ${name}!`);
+      return false;
+    }
     console.log('Correct!');
-    return true;
   }
-  console.log(`${answer} is wrong answer ;(. Correct answer was ${result}`);
-  console.log(`Let's try again, ${name}!`);
-  return false;
+
+  return console.log(`Congratulations, ${name}!`);
 };
